cb95ba8020ef0d2635b1fe1b43158522770d7ce2
Youmeng <youmenghin15@gmail.com>
2021-07-11 18:56:19 -0400

    Create SudokuGame.java

---
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,23 @@
+package sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridSate;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridSate) {
+        this.gameState = gameState;
+        this.gridSate = gridSate;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridSate() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
